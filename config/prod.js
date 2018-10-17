7f5712ad01a32c702fc959fa74679dfb6c7fbb99
Piotr Wawrzynkiewicz <pio.wawrzynkiewicz@gmail.com>
2018-10-17 19:05:33 +0200

    fix

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoDbURI: process.env.MONGO_DB_URI,
   cookieKey: process.env.COOKIE_KEY,
-  initialDbUserLogin: proccess.env.DB_USER_LOGIN,
-  initialDbUserPassword: proccess.env.DB_USER_PASSWORD
+  initialDbUserLogin: process.env.DB_USER_LOGIN,
+  initialDbUserPassword: process.env.DB_USER_PASSWORD
 };
