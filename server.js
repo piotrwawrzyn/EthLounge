af887f10ff8f025aea873a8dd615b14bfa4d765d
Piotr Wawrzynkiewicz <pio.wawrzynkiewicz@gmail.com>
2018-10-25 12:57:05 +0200

    fix

--- server.js
+++ server.js
@@ -15,18 +15,18 @@ const port = process.env.PORT || 3000;
 app.prepare().then(() => {
   const server = express();
 
-  if (process.env.NODE_ENV === 'production') {
-    server.use(express.static('client/build'));
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //    server.use(express.static('client/build'));
+  // }
 
-  server.use(proxy(`${backend}/login`), { changeOrigin: true });
-  server.use(proxy(`${backend}/logout`), { changeOrigin: true });
-  server.use(proxy(`${backend}/api/current_gambler`)), { changeOrigin: true };
-  server.use(proxy(`${backend}/admin/login`), { changeOrigin: true });
-  server.use(proxy(`${backend}/admin/logout`), { changeOrigin: true });
-  server.use(proxy(`${backend}/api/current_admin`), { changeOrigin: true });
-  server.use(proxy(`${backend}/backend/new_team`), { changeOrigin: true });
-  server.use(proxy(`${backend}/api/teams`), { changeOrigin: true });
+  server.use(proxy(`${backend}/login`));
+  server.use(proxy(`${backend}/logout`));
+  server.use(proxy(`${backend}/api/current_gambler`));
+  server.use(proxy(`${backend}/admin/login`));
+  server.use(proxy(`${backend}/admin/logout`));
+  server.use(proxy(`${backend}/api/current_admin`));
+  server.use(proxy(`${backend}/backend/new_team`));
+  server.use(proxy(`${backend}/api/teams`));
 
   server.use(handler).listen(port, err => {
     if (err) throw err;
